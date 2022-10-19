2fe3899d01400f4102d09c9255f5bb0cc29b3669
Mike <102708826+MsCrypto22@users.noreply.github.com>
2022-10-19 11:21:38 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Header from  "./header";
+import SearchIcon from "@mui/icons/Search-material";
+import HomeIcon from "@mui/icons/Home-material";
+import FlagIcon from "@mui/icons/Flag-material";
+import SubscriptionsOutLinedIcon from "@mui/icons/SubscriptionsOutLinedIcon-material";
+import StorefrontOutLinedIcon from "@mui/icons/StorefrontOutLinedIcon-material";
+import SupervisedUserCircleIcon from "@mui/icons/SupervisedUserCircleIcon-material";
+
+
+
+function App() {
+  return (
+    //BEM naming convention 
+    <div className="app">
+      <h1>lets build the facebook clone!</h1>
+
+      <Header />
+      {/* Header */}
+
+            {/* App body */}
+          {/* sidebar */}
+          {/* feed */}
+          {/* widgets*/}
+
+    </div>
+  );
+}
+
+export default App;
